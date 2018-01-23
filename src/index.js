ad2831cf4a0b3fd25db5dc4e928056ee3939b5d6
Afonso R. Costa Jr <arabelo@gmail.com>
2018-01-22 23:24:27 -0400

    Add react-redux to the project
    Also remove the raw redux usage, once we will use Provider (connect())
    to handle the redux store.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
 import reducer from './reducers';
 
 const store = createStore(reducer);
 
 ReactDOM.render(
-  <App store={store}/>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
