c5ee08c48b71cf9cda5dbd493d8c7ac2bf69713a
Afonso R. Costa Jr <arabelo@gmail.com>
2018-01-23 14:53:05 -0400

    Simplify to get the meals according to day order

--- src/components/App.js
+++ src/components/App.js
@@ -19,14 +19,8 @@ function mapStateToProps(calendar) {
   return {
     calendar: dayOrder.map((day) => ({
       day,
-      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
-        meals[meal] = calendar[day][meal]
-          ? calendar[day][meal]
-          : null
-
-        return meals
-      }, {})
-    })),
+      meals: Object.assign({}, calendar[day])
+    }))
   }
 }
 
